abe7011a13582a69be30663cf50db0d72c7138a1
Hubert Theodore <hubert@yup.com>
2016-03-06 15:20:58 -0600

    dir issue and hover styling

--- source_js/script.js
+++ source_js/script.js
@@ -1,7 +1,7 @@
 // Write any custom javascript functions here
 function tryLoadData($scope, $http, callback){
     if (!$scope.movies){
-        $http.get('/data/imdb250.json').
+        $http.get('data/imdb250.json').
           success(function(data) {
             $scope.movies = data;
             if (typeof callback !== 'undefined') {
